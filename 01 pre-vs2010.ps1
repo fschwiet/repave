4ba6fe18efb377133198f943b6e9a06c75098385
fschwiet <fschwiet@gmail.com>
2012-02-12 20:43:12 -0800

    grouping targets to run webpi command line less often

--- 01 pre-vs2010.ps1
+++ 01 pre-vs2010.ps1
@@ -1,46 +1,56 @@
+
+$webpiTargets = @(
+
 # Windows Installer
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WindowsInstaller31"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WindowsInstaller45"
+    "WindowsInstaller31",
+    "WindowsInstaller45"
 
 # Powershell
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:PowerShell"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:PowerShell2"
+    "PowerShell",
+    "PowerShell2",
 
 # .NET
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:NETFramework20SP2"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:NETFramework35"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:NETFramework4"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:JUNEAUNETFX4"
+    "NETFramework20SP2",
+    "NETFramework35",
+    "NETFramework4",
+    "JUNEAUNETFX4",
 
 # IIS
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IIS7"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:ASPNET"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:BasicAuthentication"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:DefaultDocument"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:DigestAuthentication"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:DirectoryBrowse"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:HTTPErrors"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:HTTPLogging"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:HTTPRedirection"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IIS7_ExtensionLessURLs"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IISManagementConsole"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IISManagementScriptsAndTools"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IPSecurity"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:ISAPIExtensions"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:ISAPIFilters"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:LoggingTools"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:MetabaseAndIIS6Compatibility"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:NETExtensibility"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:RequestFiltering"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:RequestMonitor"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:StaticContent"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:StaticContentCompression"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:Tracing"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WindowsAuthentication"
+    "IIS7",
+    "CGI",
+    "ASPNET",
+    "BasicAuthentication",
+    "DefaultDocument",
+    "DigestAuthentication",
+    "DirectoryBrowse",
+    "HTTPErrors",
+    "HTTPLogging",
+    "HTTPRedirection",
+    "IIS7_ExtensionLessURLs",
+    "IISManagementConsole",
+    "IISManagementScriptsAndTools",
+    "IPSecurity",
+    "ISAPIExtensions",
+    "ISAPIFilters",
+    "LoggingTools",
+    "MetabaseAndIIS6Compatibility",
+    "NETExtensibility",
+    "RequestFiltering",
+    "RequestMonitor",
+    "StaticContent",
+    "StaticContentCompression",
+    "Tracing",
+    "WindowsAuthentication",
+    "UrlRewrite2",
 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IISExpress"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:AppFabric"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:AppFabricSetup"
+    "IISExpress",
+    "AppFabric",
+    "AppFabricSetup",
+);
+
+
+
+cmd /C ("webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:" + [string]::Join(",", $webpiTargets);
 
 
 # Ninite stuff
--- 03 after you install VS2010.ps1
+++ 03 after you install VS2010.ps1
@@ -1,67 +1,61 @@
+#skipped, not sure:
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SMO 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SMO32 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCE 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCETools 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSTools 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSTools_4_0
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSToolsInstallerNew_4_0 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCLRTypes 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCLRTypes32
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLNativeClient
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLManagementStudio 
 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:IISExpress"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:AppFabric"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:AppFabricSetup"
+    # Developer tools
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:VWD_RTW"
 
-
-
-# IIS
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:UrlRewrite2"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:CGI"
-
-# # SQL Server
-
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLExpress 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLExpressAdv 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLExpressTools 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLManagementStudio 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLNativeClient 
-
-
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SMO 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SMO32 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCE 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCETools 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSTools 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSTools_4_0
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCEVSToolsInstallerNew_4_0 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCLRTypes 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLCLRTypes32
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLNativeClient
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:SQLManagementStudio 
+    # Other
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASConfigurationAPI" 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASNetFxEnvironment"
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASProcessModel"
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy" 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy_v2"
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy11" 	
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeployNoSMO"
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeployPS" 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WebMatrix" 
+    # cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WebMatrix_WHP"
 
 
 
+$webpitTargets = @(
 
+    # SQL Server
 
+    "SQLExpress",
+    "SQLExpressAdv",
+    "SQLExpressTools",
+    "SQLManagementStudio",
+    "SQLNativeClient",
 
-# Developer tools
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:VWD_RTW"
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:VISUALSTUDIO2010SP1PATCH"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:MVC2" 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:MVC3"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:MVC3Loc"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:MVC3Runtime" 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:Plan9" 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:Plan9Loc" 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:AzureAppFabricSDKV1PROD"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WindowsAzureSDK"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WindowsAzureToolsVS2010"
+    # Developer tools
+    "VISUALSTUDIO2010SP1PATCH",
+    "MVC2",
+    "MVC3",
+    "MVC3Loc",
+    "MVC3Runtime",
+    "Plan9",
+    "Plan9Loc",
+    "AzureAppFabricSDKV1PROD",
+    "WindowsAzureSDK",
+    "WindowsAzureToolsVS2010",
 
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:NodeJS"
-cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:iisnode"
+    # nodejs / iisnode
+    "NodeJS",
+    "iisnode"
+);
 
-# Other
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASConfigurationAPI" 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASNetFxEnvironment"
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WASProcessModel"
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy" 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy_v2"
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeploy11" 	
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeployNoSMO"
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WDeployPS" 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WebMatrix" 
-# cmd /C "webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:WebMatrix_WHP"
+cmd /C ("webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:" + [string]::Join(",", $webpitTargets);
 
 # R#
 cinst resharper
