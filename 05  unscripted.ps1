5fd5cae6eb2243f1045d03a70dbe3ff00f480065
fschwiet <fschwiet@gmail.com>
2012-02-19 21:06:52 -0800

    meh

--- 05  unscripted.ps1
+++ 05  unscripted.ps1
@@ -3,4 +3,10 @@
 
 install kiln client from https://ichc.kilnhg.com/Tools/Downloads/KilnClient
 
+
+for appfabric:
+  create a user
+  create a shared directory with permissions for that user and current user
+
+
 "@
