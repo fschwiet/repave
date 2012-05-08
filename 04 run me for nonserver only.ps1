b1e17412fbf44d3d12dd7a38785efbe90f5fc33c
fschwiet <fschwiet@gmail.com>
2012-05-08 00:30:40 -0700

    adding IIS back later

--- 04 run me for nonserver only.ps1
+++ 04 run me for nonserver only.ps1
@@ -8,3 +8,34 @@ cinst resharper
 # some registry changes
 regedit /s '.\registry\DisableVisualStudioAttachWarning.reg'
 regedit /s '.\registry\swapCapsLockAndLWin.reg'
+
+$iisComponents = @(
+    "IIS7",
+    "CGI",
+    "ASPNET",
+    "BasicAuthentication",
+    "DefaultDocument",
+    "DigestAuthentication",
+    "DirectoryBrowse",
+    "HTTPErrors",
+    "HTTPLogging",
+    "HTTPRedirection",
+    "IIS7_ExtensionLessURLs",
+    "IISManagementConsole",
+    "IISManagementScriptsAndTools",
+    "IPSecurity",
+    "ISAPIExtensions",
+    "ISAPIFilters",
+    "LoggingTools",
+    "MetabaseAndIIS6Compatibility",
+    "NETExtensibility",
+    "RequestFiltering",
+    "RequestMonitor",
+    "StaticContent",
+    "StaticContentCompression",
+    "Tracing",
+    "WindowsAuthentication",
+    "UrlRewrite2"
+);
+
+$iisComponents | % { cinst $_ -source webpi }
