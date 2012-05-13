b908e7d31ff2fcaa40cc7fb0b37c551d8224df6b
fschwiet <fschwiet@gmail.com>
2012-05-13 15:31:08 -0700

    adding windows update config

--- 01 run me first.ps1
+++ 01 run me first.ps1
@@ -24,6 +24,11 @@ cmd /C ("webpicmdline\webpicmdline.exe /AcceptEula /SuppressReboot /Products:" +
 cmd /C "ninite\ninite.core.exe"
 
 
+#configure windows update to download but prompt before installing
+. .\Misc\WindowsUpdateConfig.ps1
+Set-WindowsUpdateConfig 3 -IncludeRecommended $false
+
+
 # installing chocolatey requires restarting the console, so do this before next step
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 
