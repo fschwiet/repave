10a4bbe4a4b0177df791ac5f61a7bc0935e16985
fschwiet <fschwiet@gmail.com>
2012-09-03 20:41:45 -0700

    moving IIS installation to first script

--- 01 run me first.ps1
+++ 01 run me first.ps1
@@ -8,3 +8,34 @@ cmd /C "ninite\ninite.core.exe"
 # installing chocolatey requires restarting the console, so do this before next step
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 
+function InstallFeature($name) {
+	Write-Host "Installing $name"
+	cmd /c "ocsetup $name /passive"
+}
+
+Write-Host "Beginning installation"
+InstallFeature IIS-WebServerRole
+	InstallFeature IIS-WebServer
+		InstallFeature IIS-CommonHttpFeatures
+			InstallFeature IIS-DefaultDocument
+			InstallFeature IIS-DirectoryBrowsing
+			InstallFeature IIS-HttpErrors
+			InstallFeature IIS-HttpRedirect
+			InstallFeature IIS-StaticContent
+		InstallFeature IIS-HealthAndDiagnostics
+			InstallFeature IIS-CustomLogging
+			InstallFeature IIS-HttpLogging
+			InstallFeature IIS-HttpTracing
+			InstallFeature IIS-LoggingLibraries
+		InstallFeature IIS-Security
+			InstallFeature IIS-RequestFiltering
+			InstallFeature IIS-WindowsAuthentication
+		InstallFeature IIS-ApplicationDevelopment
+			InstallFeature IIS-NetFxExtensibility
+			InstallFeature IIS-ISAPIExtensions
+			InstallFeature IIS-ISAPIFilter
+			InstallFeature IIS-ASPNET
+	InstallFeature IIS-WebServerManagementTools 
+		InstallFeature IIS-ManagementConsole 
+		InstallFeature IIS-ManagementScriptingTools
+		
--- 03 run me controversial.ps1
+++ 03 run me controversial.ps1
@@ -5,38 +5,3 @@ cmd /C "ninite\ninite.client.exe"
 # some registry changes
 regedit /s '.\registry\swapCapsLockAndLWin.reg'
 
-function InstallFeature($name) {
-	Write-Host "Installing $name"
-	cmd /c "ocsetup $name /passive"
-}
-
-Write-Host "Beginning installation"
-InstallFeature IIS-WebServerRole
-	InstallFeature IIS-WebServer
-		InstallFeature IIS-CommonHttpFeatures
-			InstallFeature IIS-DefaultDocument
-			InstallFeature IIS-DirectoryBrowsing
-			InstallFeature IIS-HttpErrors
-			InstallFeature IIS-HttpRedirect
-			InstallFeature IIS-StaticContent
-		InstallFeature IIS-HealthAndDiagnostics
-			InstallFeature IIS-CustomLogging
-			InstallFeature IIS-HttpLogging
-			InstallFeature IIS-HttpTracing
-			InstallFeature IIS-LoggingLibraries
-		InstallFeature IIS-Security
-			InstallFeature IIS-RequestFiltering
-			InstallFeature IIS-WindowsAuthentication
-		InstallFeature IIS-ApplicationDevelopment
-			InstallFeature IIS-NetFxExtensibility
-			InstallFeature IIS-ISAPIExtensions
-			InstallFeature IIS-ISAPIFilter
-			InstallFeature IIS-ASPNET
-	InstallFeature IIS-WebServerManagementTools 
-		InstallFeature IIS-ManagementConsole 
-		InstallFeature IIS-ManagementScriptingTools
-
-#import-module WebAdministration
-
-& $env:windir\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -i
-& $env:windir\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe -i
