379a300b4c19ee95748dbbf503dcb4846200f42d
Frank Schwieterman <fschwiet@gmail.com>
2012-09-03 20:36:20 -0700

    use alternate method to install IIS

--- 03 run me controversial.ps1
+++ 03 run me controversial.ps1
@@ -5,36 +5,38 @@ cmd /C "ninite\ninite.client.exe"
 # some registry changes
 regedit /s '.\registry\swapCapsLockAndLWin.reg'
 
-$iisComponents = @(
-    "IIS7",
-    "CGI",
-    "ASPNET",
-    "BasicAuthentication",
-    "DefaultDocument",
-    "DigestAuthentication",
-    "DirectoryBrowse",
-    "HTTPErrors",
-    "HTTPLogging",
-    "HTTPRedirection",
-    "IIS7_ExtensionLessURLs",
-    "IISManagementConsole",
-    "IISManagementScriptsAndTools",
-    "IPSecurity",
-    "ISAPIExtensions",
-    "ISAPIFilters",
-    "LoggingTools",
-    "MetabaseAndIIS6Compatibility",
-    "NETExtensibility",
-    "RequestFiltering",
-    "RequestMonitor",
-    "StaticContent",
-    "StaticContentCompression",
-    "Tracing",
-    "WindowsAuthentication",
-    "UrlRewrite2"
-);
+function InstallFeature($name) {
+	Write-Host "Installing $name"
+	cmd /c "ocsetup $name /passive"
+}
 
+Write-Host "Beginning installation"
+InstallFeature IIS-WebServerRole
+	InstallFeature IIS-WebServer
+		InstallFeature IIS-CommonHttpFeatures
+			InstallFeature IIS-DefaultDocument
+			InstallFeature IIS-DirectoryBrowsing
+			InstallFeature IIS-HttpErrors
+			InstallFeature IIS-HttpRedirect
+			InstallFeature IIS-StaticContent
+		InstallFeature IIS-HealthAndDiagnostics
+			InstallFeature IIS-CustomLogging
+			InstallFeature IIS-HttpLogging
+			InstallFeature IIS-HttpTracing
+			InstallFeature IIS-LoggingLibraries
+		InstallFeature IIS-Security
+			InstallFeature IIS-RequestFiltering
+			InstallFeature IIS-WindowsAuthentication
+		InstallFeature IIS-ApplicationDevelopment
+			InstallFeature IIS-NetFxExtensibility
+			InstallFeature IIS-ISAPIExtensions
+			InstallFeature IIS-ISAPIFilter
+			InstallFeature IIS-ASPNET
+	InstallFeature IIS-WebServerManagementTools 
+		InstallFeature IIS-ManagementConsole 
+		InstallFeature IIS-ManagementScriptingTools
 
-$iisComponents | % { webpicmd /products:$_ }
+#import-module WebAdministration
 
-
+& $env:windir\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -i
+& $env:windir\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe -i
