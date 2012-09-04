4b1331a1611475f8ce6ef67770699e1c8a8da0eb
fschwiet <fschwiet@gmail.com>
2012-09-03 20:21:47 -0700

    disabling thing that didn't work in windows 8

--- 01 run me first.ps1
+++ 01 run me first.ps1
@@ -1,9 +1,9 @@
 
 cmd /C "ninite\ninite.core.exe"
 
-#configure windows update to download but prompt before installing
-. .\Misc\WindowsUpdateConfig.ps1
-Set-WindowsUpdateConfig 3 -IncludeRecommended $false
+##configure windows update to download but prompt before installing
+#. .\Misc\WindowsUpdateConfig.ps1
+#Set-WindowsUpdateConfig 3 -IncludeRecommended $false
 
 # installing chocolatey requires restarting the console, so do this before next step
 iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
